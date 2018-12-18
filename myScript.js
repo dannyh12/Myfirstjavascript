83e2e3ffc85fb135397d322d7c2a699994cc7d4a
dannyh12 <twerking@gmail.com>
2018-12-18 08:29:25 -0700

    Added a math function
    :)

--- myScript.js
+++ myScript.js
@@ -1,3 +1,24 @@
+//This function changed the text in the demo paragraph 
+
 function myFunction () {
     document.getElementById('demo').innerHTML = "Paragraph Changed";
+}
+
+//Defining variables
+
+//var x = 5;
+//var y = 6;
+//var z = x + y;
+
+
+var x, y, z;
+var x = 5;
+var y = 6;
+var z = x + y;
+
+//This function changes the text in the math paragraph
+
+function mathFunction () {
+    document.getElementById('math').innerHTML =
+        z;
 }
