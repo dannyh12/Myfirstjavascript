7374f703a405b851440fabbd7f243ad8b20b866c
dannyh12 <twerking@gmail.com>
2019-01-15 08:31:28 -0700

    added great stuff
    finished project

--- myScript.js
+++ myScript.js
@@ -1,24 +1,13 @@
-//This function changed the text in the demo paragraph 
+//This function changes the text in the demo paragraph
 
 function myFunction () {
     document.getElementById('demo').innerHTML = "Paragraph Changed";
 }
 
-//Defining variables
 
-//var x = 5;
-//var y = 6;
-//var z = x + y;
-
-
-var x, y, z;
-var x = 5;
-var y = 6;
-var z = x + y;
 
 //This function changes the text in the math paragraph
 
-function mathFunction () {
-    document.getElementById('math').innerHTML =
-        z;
-}
+function mathFunction (x,y) {
+    return x + y
+    }
